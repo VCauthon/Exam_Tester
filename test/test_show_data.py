ea76572b88b7526f8bf0a00fbcf9a17cccd29f40
Victor Mas Montserrat <victormasmon@gmail.com>
2022-09-01 00:06:42 +0200

    Rename subclass answer_questions.py

---
+++ test/test_show_data.py
@@ -0,0 +1,16 @@
+import pytest
+import exam_tester.show_data
+
+'''
+    Verifies all the subclasses inside show_data.py
+'''
+
+# Section to create all the fixtures to mock all the test 
+
+    #@pytest.fixture()
+
+# Test for the 
+
+# 
+
+#
