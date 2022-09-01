06572905806d6ad372360d228661c57bac58945a
Victor Mas Montserrat <victormasmon@gmail.com>
2022-09-01 13:38:01 +0200

    Started read_data method in import_questions

--- exam_tester/io.py
+++ exam_tester/io.py
@@ -1,20 +1,24 @@
 from abc import ABC, abstractclassmethod
-from exam_tester.entity import question
+from exam_tester.entities import question
 
 class io(ABC):
     '''
     Super class with all the classes used to import and export data about the questions
     '''
 
-    def __init__(self, course) -> None:
+    def __init__(self, course: dict) -> None:
         '''
             Sets the necessary data to read the questions of courses
         '''
         super().__init__()
         self.course = course
+        self.error_detected = "None errors detected in the process"
 
     @abstractclassmethod
-    def read_data(self) -> question:
+    def read_data(self) -> list(question):
+        '''
+            Abstract method to read CSV and return a pandas with 
+        '''
         pass
 
     @abstractclassmethod
