45f447f30ed94e6348b077fe078f3eedfd6b53fd
VCauthon <victormasmon@gmail.com>
2022-09-21 23:57:45 +0200

    First test of the clases

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 '''
-    1. Shows all the courses that exist in the dir courses (there are will be saved all the questions in CSV)
+    1. Shows all the courses that exist in the dir courses
     2. From the select one, loads all the questions saved
     3. Shows how many questions exists by that course
     4. Shows all the questions that exists by course
     5. Shows the % of exit from the user
 '''
+
+from exam_tester import data_validator as dv
+
+# print(dv.data_validator.__subclasses__())
+# print(dv.data_validator.EXISTING_COURSES)
+
+a = dv.questions_loader(course=list(
+    dv.data_validator.EXISTING_COURSES.keys())[0])
+result_1 = a.initial_execution()
+result_2 = a.import_new_questions()
+print(a.working_course)
