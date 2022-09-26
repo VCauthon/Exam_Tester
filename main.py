44d0a0407df04d1e1084f3c0f1b88cedab57870b
Victor Mas Montserrat <victormasmon@gmail.com>
2022-09-26 21:03:20 +0200

    Add notations

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from exam_tester import QuestionManager as dv
 # print(dv.data_validator.__subclasses__())
 # print(dv.data_validator.EXISTING_COURSES)
 
+
+# TODO: Add a validation to validate the imported questions if there are a load_questions.csv file and there exists row in it
+
 a = dv.questions_loader(course=list(
     dv.data_validator.EXISTING_COURSES.keys())[0])
 result_1 = a.initial_execution()
